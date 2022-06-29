93bca10a338859c8d81de84267042330019a2455
omura-chisae <105641437+omura-chisae@users.noreply.github.com>
2022-06-30 01:32:48 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,38 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { memo, ReactNode, VFC } from "react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="18px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="small" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
